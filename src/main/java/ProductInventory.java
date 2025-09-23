eeb786987018a6278b5ddfc429b430280978d073
mrs-liliia-kutova <lilqatester@gmail.com>
2025-09-23 14:43:57 +0200

    Creating Product inventory class

---
+++ src/main/java/ProductInventory.java
@@ -0,0 +1,29 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProductInventory {
+
+    private List<String> products = new ArrayList<>();
+
+    public void addProduct(String product){
+        products.add(product);
+    }
+
+    public void removeProduct(String name) throws OutOfStockException {
+        if (!products.remove(name)) {
+            throw new OutOfStockException(" Product " + name + " is out of stock ");
+        }
+    }
+    public void sortProducts() {
+            Collections.sort(products);
+        }
+
+        public List<String> getProducts(){
+            return new ArrayList<>(products);
+        }
+    }
+
+
